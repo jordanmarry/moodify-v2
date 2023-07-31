b0e1d667fd3b3d2c0f26c55cc9a493e0175daf84
Jordan <jmarry@terpmail.umd.edu>
2023-07-30 20:31:32 -0400

    Code Clean Up

--- client/src/components/SongCard.js
+++ client/src/components/SongCard.js
@@ -1,7 +1,4 @@
 import React, { useEffect } from "react";
-import Link from 'next/link';
-
-const MAX_CHARACTERS = 20;
 
 const truncateText = (text, maxLength) => {
     if (text.length > maxLength) {
@@ -13,9 +10,6 @@ const truncateText = (text, maxLength) => {
 const SongCard = ({ imgSrc, song, songLink, artistList, 
     album, albumLink }) => {
 
-    const truncatedSong = truncateText(song, MAX_CHARACTERS);
-    const truncatedAlbum = truncateText(album, MAX_CHARACTERS);
-
     return (
         <div className="flex flex-col items-center">
             <div className="border border-off-white bg-off-white">
@@ -26,14 +20,18 @@ const SongCard = ({ imgSrc, song, songLink, artistList,
                         </a>
                     </div>
                     <div className="text-grey pt-2">
-                        <a href={songLink} className="text-xl w-72 block overflow-hidden whitespace-nowrap overflow-ellipsis">{song}</a>
-                        <a href={albumLink} className="text-lg w-60 block overflow-hidden whitespace-nowrap overflow-ellipsis">{album}</a>
+                        <div>  
+                            <a href={songLink} className="text-xl w-72 block overflow-hidden whitespace-nowrap overflow-ellipsis">{song}</a>
+                        </div>
+                        <div>
+                            <a href={albumLink} className="text-lg w-72 block overflow-hidden whitespace-nowrap overflow-ellipsis">{album}</a>
+                        </div>
                         {artistList && artistList.length > 0 && (
-                            <div className="">
+                            <div className="w-72 block overflow-hidden whitespace-nowrap overflow-ellipsis">
                                 {artistList.map((artistObj, index) => (
                                     <React.Fragment key={index}>
                                         <a href={artistObj.artistLink} className="text-sm">
-                                        {truncateText(artistObj.artist, MAX_CHARACTERS)}
+                                            {artistObj.artist}
                                         </a>
                                         {index !== artistList.length - 1 && ", "}
                                     </React.Fragment>
