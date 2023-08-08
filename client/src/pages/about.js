00b2e7e66b89fcbafc0b2ab6156a26eb81d56345
Jordan <jmarry@terpmail.umd.edu>
2023-08-08 11:31:47 -0400

    created the other pages

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -3,12 +3,21 @@ import Head from 'next/head';
 
 const Home = () => {
     return (
-        <div className='h-screen'>
+        <div className='h-screen items-center flex justify-center pt-10 sm:pt-0'>
             <Head>
                 <title>Moodify | About</title>
             </Head>
 
-            HI 
+            <div className="text-2xl text-center text-off-white sm:w-2/5">
+                <p className="text-4xl pb-10 text-dark-blue">
+                    Welcome to Moodify! 
+                </p>
+                <p>
+                    I created this app to allow people to be able to express 
+                    their emotions and feelings to friends, significant others, or 
+                    family when words aren't enough.
+                </p>
+            </div>
         </div>
     )
 }
