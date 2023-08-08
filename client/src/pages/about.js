63e5cc14c8bbd64dd6b95aef01908086c20cb811
Jordan <jmarry@terpmail.umd.edu>
2023-08-07 21:32:33 -0400

    added the rest of the navbar pages

---
+++ client/src/pages/about.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Head from 'next/head';
+
+const Home = () => {
+    return (
+        <div className='h-screen'>
+            <Head>
+                <title>Moodify | About</title>
+            </Head>
+
+            HI 
+        </div>
+    )
+}
+
+export default Home;
