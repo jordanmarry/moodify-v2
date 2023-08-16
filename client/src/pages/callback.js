f6e040a125f697e1b81ecb2f368f91d1bec07d24
Jordan <jmarry@terpmail.umd.edu>
2023-08-16 12:49:34 -0400

    fixed the promise.

--- client/src/pages/callback.js
+++ client/src/pages/callback.js
@@ -23,35 +23,35 @@ const Callback = () => {
       window.localStorage.setItem("token", token)
     }
 
+    // Retrieving Account Info from Spotify API
+    const getAccountInfo = async () => {
+      try {
+        // Calling Spotify API for the user's info
+        const {data} = await axios.get('https://api.spotify.com/v1/me', {
+          headers: {
+            Authorization: `Bearer ${window.localStorage.getItem("token")}`
+          }
+        })
+
+        window.localStorage.setItem("displayName", data.display_name)
+        window.localStorage.setItem("userId", data.id)
+
+        // Posting the data to /user for the backend to then push this into DB
+        const url = "http://localhost:5050/user"
+        axios.post(url, {data})
+
+        // Moves the User to the Home Page
+        window.location.href = '/home'
+
+      } catch (error) {
+        console.error('Error while retrieving profile info', error)
+      }
+      
+    }
+
     // Retrieving Account Information, Display Name and User ID.
     getAccountInfo()
-
-    // Moves the User to the Home Page
-    window.location.href = '/home'
   }, []);
-
-  // Retrieving Account Info from Spotify API
-  const getAccountInfo = async () => {
-    try {
-      // Calling Spotify API for the user's info
-      const {data} = await axios.get('https://api.spotify.com/v1/me', {
-        headers: {
-          Authorization: `Bearer ${window.localStorage.getItem("token")}`
-        }
-      })
-
-      window.localStorage.setItem("displayName", data.display_name)
-      window.localStorage.setItem("userId", data.id)
-
-      // Posting the data to /user for the backend to then push this into DB
-      const url = "http://localhost:5050/user"
-      axios.post(url, {data})
-
-    } catch (error) {
-      console.error('Error while retrieving profile info', error)
-    }
-    
-  }
 
   return (
     <div className='h-screen'>
