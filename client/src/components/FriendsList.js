cb73a0c704019bdb48d39bc0cb13ea33c91f1807
Jordan Marry <jmarry@terpmail.umd.edu>
2023-09-20 18:56:34 -0400

    Start to friends list

--- client/src/components/FriendsList.js
+++ client/src/components/FriendsList.js
@@ -2,7 +2,13 @@ import React, { useEffect } from "react";
 import Image from "next/image";
 
 const FriendsList = ({ friendsList }) => {
-    
+    return (
+        <div>
+            <ul>
+                
+            </ul>
+        </div>
+    )
 };
 
 export default FriendsList;
